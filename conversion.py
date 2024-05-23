557ae0d4ef709a787257e983af5cdcc676b4dccf
Michael Li <michaaeli@Michaels-MacBook-Air-5.local>
2024-05-23 17:30:20 -0400

    added conversion from llh to xyz and vice versa methods

--- conversion.py
+++ conversion.py
@@ -1,33 +1,63 @@
 import json
 import math
+import numpy as np
+import pyproj
 
 class llhRotation():
-    def __init__(self, x, y, z, bear):
+    def __init__(self, x, y, z, bear, lat, lon, h):
         self.x = x
         self.y = y
         self.z = z
         self.bear = bear
+        self.lat = lat
+        self.lon = lon
+        self.h = h
         #self.conversion-methods()
+        self.xyztollh()
 
-    def calc_rotation():
-        # 2. Rotation Matrix for Bearing
-        local_coord = [x, y, z]
-        rot_matrix = [[math.cos(bear), math.sin(bear), 0],
-                  [-(math.sin(bear)), math.cos(bear), 0],
-                  [0, 0, 1]]
-        # 3. Rotate Local Coordinates
-            # multiply values
+    def calc_rotation(self):
+        local_coord = np.array([self.x, self.y, self.z])
+        bear_rad = math.radians(self.bear)
+        rot_matrix = np.array([[math.cos(bear_rad), -(math.sin(bear_rad)), 0],
+                      [math.sin(bear_rad), math.cos(bear_rad), 0],
+                      [0, 0, 1]])
+        print(rot_matrix)
         for i in range(3):
-            for j in range(3):
-                rot_matrix[i][j] *= local_coord[i]
-            # rotate matrix
-        for i in range(3):
-            for j in range(3):
-                rot_matrix[i][j], rot_matrix[j][i] == rot_matrix[j][i], rot_matrix[i][j]
-        rot_coord = [] * len(local_coord)
-        for k in range(3):
-            for l in range(3):
-                #add up stuff
+            rot_matrix[:, i] *= local_coord[i]
+        print(rot_matrix)
+        rot_coord = np.sum(rot_matrix, axis = 1)
+        print("this is root coord ",rot_coord)
+        return rot_coord
+
+    def llhtoxyz(self):
+        transformer = pyproj.Transformer.from_crs(
+            {"proj":'latlong', "ellps":'WGS84', "datum":'WGS84'},
+            {"proj":'geocent', "ellps":'WGS84', "datum":'WGS84'},
+            )
+        x, y, z = transformer.transform(self.lon,self.lat,self.h,radians = False)
+        lidar_coord = np.array([x, y, z])
+        print("this is lidar coord ", lidar_coord)
+        return lidar_coord
+
+    def translateToGlobal(self):
+        rot_coord = self.calc_rotation()
+        lidar_coord = self.llhtoxyz()
+        combined_coord = rot_coord + lidar_coord 
+        print(combined_coord)
+        return combined_coord
+
+    def xyztollh(self):
+        global_coord = self.translateToGlobal()
+        transformer2 = pyproj.Transformer.from_crs(
+            {"proj":'geocent', "ellps":'WGS84', "datum":'WGS84'},
+            {"proj":'latlong', "ellps":'WGS84', "datum":'WGS84'},
+            )
+        lon, lat, h =transformer2.transform(global_coord[1],global_coord[0],global_coord[2],radians=False)
+        print("final: ", lat, lon, h)
+
+test = llhRotation(5, 10, 2, 45, 37.7749, -122.4194, 30)
+print(test)
+
 
 
     #def convert_xyz_llh():

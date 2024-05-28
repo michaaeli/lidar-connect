7f5384da7253dc804c3ea3ff835652dbe663acee
Michael Li <michaaeli@Michaels-MacBook-Air-5.local>
2024-05-28 17:33:20 -0400

    finished conversion, started tests

--- conversion.py
+++ conversion.py
@@ -4,29 +4,29 @@ import numpy as np
 import pyproj
 
 class llhRotation():
-    def __init__(self, x, y, z, bear, lat, lon, h):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.bear = bear
+    def __init__(self, lat, lon, h, bear):
         self.lat = lat
         self.lon = lon
         self.h = h
-        #self.conversion-methods()
-        self.xyztollh()
+        self.bear = math.radians(bear)
+        
+        self.converted_lidar = self.llhtoxyz()
 
-    def calc_rotation(self):
-        local_coord = np.array([self.x, self.y, self.z])
-        bear_rad = math.radians(self.bear)
-        rot_matrix = np.array([[math.cos(bear_rad), -(math.sin(bear_rad)), 0],
-                      [math.sin(bear_rad), math.cos(bear_rad), 0],
+    def local_to_global(self, x, y, z):
+        coord_sum = self.calc_rotation(x, y, z) + self.llhtoxyz()
+        final_coord = self.xyztollh(coord_sum)
+        return final_coord
+
+    def calc_rotation(self, x, y, z):
+        local_coord = np.array([x, y, z])
+        rot_matrix = np.array([[math.cos(self.bear), -(math.sin(self.bear)), 0],
+                      [math.sin(self.bear), math.cos(self.bear), 0],
                       [0, 0, 1]])
         for i in range(3):
             rot_matrix[:, i] *= local_coord[i]
         rot_coord = np.sum(rot_matrix, axis = 1)
-        print(rot_coord)
         return rot_coord
-
+    
     def llhtoxyz(self):
         transformer = pyproj.Transformer.from_crs(
             {"proj":'latlong', "ellps":'WGS84', "datum":'WGS84'},
@@ -35,22 +35,13 @@ class llhRotation():
         x, y, z = transformer.transform(self.lon,self.lat,self.h,radians = False)
         lidar_coord = np.array([x, y, z])
         return lidar_coord
-
-    def translateToGlobal(self):
-        rot_coord = self.calc_rotation()
-        lidar_coord = self.llhtoxyz()
-        combined_coord = rot_coord + lidar_coord 
-        return combined_coord
-
-    def xyztollh(self):
-        global_coord = self.translateToGlobal()
+    
+    def xyztollh(self, coord):
         transformer2 = pyproj.Transformer.from_crs(
             {"proj":'geocent', "ellps":'WGS84', "datum":'WGS84'},
             {"proj":'latlong', "ellps":'WGS84', "datum":'WGS84'},
             )
-        lon, lat, h =transformer2.transform(global_coord[0],global_coord[1],global_coord[2],radians=False)
+        lon, lat, h = transformer2.transform(coord[0],coord[1],coord[2],radians=False)
         final_coord = [lat, lon, h]
         return final_coord 
 
-test = llhRotation(5, 10, 2, 45, 37.7749, -122.4194, 30)
-print(test)
--- test.py
+++ test.py
@@ -1,16 +1,34 @@
 import unittest
 from conversion import llhRotation
-import numpy
+import numpy as np
 
 class TestCalculations(unittest.TestCase):
     def test_calc_rotation(self):
         # calculation = llhRotation(10 , 20, 4, 90, 60, 120, 30)
-        # self.assertEqual(calculation.calc_rotation(), [-1598559.793, 2768786.781, 5500503.115], 'The rotated coordinates are wrong')
-        arr = np.array([-3.53553391, 10.60660172,  2])
         calculation = llhRotation(5, 10, 2, 45, 37.7749, -122.4194, 30)
-        self.assertEqual(calculation.calc_rotation(), [-3.53553391, 10.60660172,  2], 'The rotated coordinates are wrong')
+        np.testing.assert_array_almost_equal(
+            calculation.calc_rotation(), 
+            [-3.53553391, 10.60660172, 2], 
+            decimal=6, 
+            err_msg='The rotated coordinates are wrong'
+        )
+    def test_llhtoxyz(self):
+        # calculation = llhRotation(10 , 20, 4, 90, 60, 120, 30)
+        calculation = llhRotation(5, 10, 2, 45, 37.7749, -122.4194, 30)
+        np.testing.assert_array_almost_equal(
+            calculation.llhtoxyz(), 
+            [-2706188, -4261080, 3885744], 
+            decimal=0, 
+            err_msg='The cartesian coordinates are wrong'
+        )
 
 if __name__ == '__main__':
     unittest.main()
+
+#coordinates for test
+    # 1. "lat": 43.784911565845896, "lng": 77.3601425315789 - top right
+    # 2. "lat": 42.33570713319381, "lng": -71.16874694824219 - top left
+    # 3. "lat": -33.58778202947038, "lng": -70.45620140178822 - bottom left
+    # 4. "lat": -33.58778202947038, "lng": -70.45620140178822 - bottom right
 
 

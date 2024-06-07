8df4e7c0b477a3b94cbe9baf266626e6404ae1ae
Michael Li <michaaeli@Michaels-MacBook-Air-5.local>
2024-06-07 16:33:57 -0400

    changed names of class and experimental method to match its use, added instance of beairng to constructor, turned x & y into arguments for method

--- conversion.py
+++ conversion.py
@@ -1,27 +1,25 @@
 import math
 import numpy as np
 import pyproj
-from local_coords_to_global import xy2latlon, compute_destination_point
+from local_coords_to_global import compute_destination_point
 
-class getLLH():
-    def __init__(self, lat, lon, lat2, lon2, x, y):
+class convert_object():
+    def __init__(self, lat, lon, lat2, lon2):
         self.lat = lat
         self.lon = lon
         self.lat2 = lat2
         self.lon2 = lon2
-        self.x = x
-        self.y = y
+        self.az = self.get_bearing()
         
     def get_bearing(self):
         geodesic = pyproj.Geod(ellps='WGS84')
         fwd_azimuth,back_azimuth,distance = geodesic.inv(self.lon, self.lat, self.lon2, self.lat2)
         return fwd_azimuth 
     
-    def experimental2(self):
-        az = self.get_bearing()
-        d = math.sqrt(self.x*self.x + self.y*self.y)
-        final_bearing = az
-        cos_alpha = self.x/d
+    def get_final_coords(self, x, y):
+        d = math.sqrt(x*x + y*y)
+        final_bearing = self.az
+        cos_alpha = x/d
         alpha = np.rad2deg(np.arccos(cos_alpha))
         final_bearing += 90 - alpha
 

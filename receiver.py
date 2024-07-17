fe416840ab67b270b7c5a0739c028c220a258234
Michael Li <michaaeli@Michaels-MacBook-Air-5.local>
2024-07-16 19:01:03 -0700

    unfinished test process

--- receiver.py
+++ receiver.py
@@ -6,14 +6,13 @@ import logging
 import queue
 
 class Data():
+    #TODO Implement
     def __init__(self) -> None:
         pass
 
+    #TODO Implement
     def get_data(self) -> DetectedObject:
-        data = DetectedObject(123, 12, 12, 0, datetime.datetime.now(), 3)
-        if(len(list)<3):
-            self.logger.error("at least 1 entry missing")
-        return data
+        return None
 
 class Receiver():
     def __init__(self, logger, data_source : Data, converter : ConvertObject, result_queue : queue.Queue):
@@ -45,20 +44,5 @@ class Receiver():
         self.convert()
         self.enqueue()
 
-# test 3
-# x = getLLH(33.730979, -117.937358, 33.736142, -117.943838, -1142.63, 1110.45)
-# Expected 33.737889, -117.946033
-# Actual   33.730433665414175, -117.95457518440682
-if __name__ == "__main__":
-    
-    coords = Config()
-    lidar_pos = coords.get_lidar_pos()
-    
-    data1 = (lidar_pos[0], lidar_pos[1], lidar_pos[2], lidar_pos[3])
-    arr = []
-    i = ConvertObject(lidar_pos[0], lidar_pos[1], lidar_pos[2], lidar_pos[3])
 
-    x = Receiver(data1, i, arr)
-    print(x.convert())
-    print(x.enqueue())
 
--- receiver_test.py
+++ receiver_test.py
@@ -4,6 +4,7 @@ from data.detected_objects import DetectedObject
 from conversion import ConvertObject
 from datetime import datetime
 from unittest.mock import Mock
+from unittest.mock import MagicMock
 import logging
 import numpy as np
 import queue
@@ -18,34 +19,64 @@ class TestReceiver(unittest.TestCase):
     def test_convert(self):
         #coordinates for test: 33.727375, -117.876614, 33.730769, -117.876566, (x,y): (375.8184, 289.865), result = [33.72994163364332, -117.87251332772777]
     
+        #Arrange
         mock_data = DetectedObject(123, 375.8184, 289.865, 0, datetime.now(), 3)
         self.data.get_data.return_value = mock_data
-
         calculation = Receiver(self.logger, self.data, self.converter, self.result_queue)
-
+        #Act
         is_close = np.isclose(
             calculation.convert(), 
             [33.72994163364332, -117.87251332772777]
         )
-
+        #Assert 
         print(calculation)
         print(is_close)
         print(np.all(is_close))
         self.assertTrue(np.all(is_close))
 
     def test_enqueue(self):
+        #Arrange
         mock_data = DetectedObject(123, 375.8184, 289.865, 0, datetime.now(), 3)
         self.data.get_data.return_value = mock_data
-
         calculation = Receiver(self.logger, self.data, self.converter, self.result_queue)
         q = calculation.enqueue()
-
+        #Act
         is_close = np.isclose(
             q.get(), 
             [33.72994163364332, -117.87251332772777]
         )
-        
+        #Assert
         self.assertTrue(np.all(is_close))
+
+    def test_process(self):
+        # #Arrange
+        # mock_data = DetectedObject(123, 375.8184, 289.865, 0, datetime.now(), 3)
+        # self.data.get_data.return_value = mock_data
+        # calculation = Receiver(self.logger, self.data, self.converter, self.result_queue)
+        # #Act
+        # #Assert
+        
+        receiver = Receiver(self.logger, self.data, self.converter, self.result_queue)
+        self.mock_detected_object = Mock(spec=DetectedObject)
+        self.mock_detected_object.x = 375.8184
+        self.mock_detected_object.y = 289.865
+        self.mock_detected_object.set_global_coordinates = Mock()
+
+        self.converter = Mock(spec=ConvertObject)
+        self.converter.get_final_coords.return_value = (33.72994163364332, -117.87251332772777)
+        # Arrange
+
+        # Act
+        receiver.process()
+
+        # Assert
+        self.data.get_data.assert_called_once()
+        self.converter.get_final_coords.assert_called_once_with(375.8184, 289.865)
+        self.mock_detected_object.set_global_coordinates.assert_called_once_with(33.72994163364332, -117.87251332772777, 0.0)
+        self.assertEqual(self.result_queue.qsize(), 1)
+        self.logger.info.assert_called_with("new entry queued")
+
+    
 
 if __name__ == '__main__':
     unittest.main()
